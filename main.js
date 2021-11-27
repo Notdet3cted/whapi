7ed6b1e7242de4b329f48eca012b17f9219a5f91
Notdet3cted <whoa75039@gmail.com>
2021-11-27 10:24:45 +0700

    tambah button

--- main.js
+++ main.js
@@ -38,14 +38,61 @@ connect().catch((err) => {
 });
 
 io.on("connection", (socket) => {
-    conn.on("qr", (qr) => {
+	socket.on('ready', () => {
+		if (fs.existsSync('./wabot-session.json') && conn.state == 'open') {
+			io.emit('authenticated', conn.user.name + " </br>(" + conn.user.jid + ")")
+			socket.emit('message', 'Sudah konek!')
+		} else {
+			io.emit('loader', '')
+			socket.emit('message', 'Please wait..')
+			connect()
+		}
+	})
+
+	conn.on("qr", (qr) => {
 		socket.emit('message', 'Getting QR Code')
 		qrcode.toDataURL(qr, (err, url) => {
 			socket.emit('message', 'QR Code received, scan please!')
 			socket.emit("qr", url);
 		});
-        
-    });
+	});
+
+	socket.on('logout', () => {
+		if (fs.existsSync("./wabot-session.json")) {
+			conn.close()
+			conn.clearAuthInfo();
+			fs.unlinkSync("./wabot-session.json");
+			socket.emit('isdelete', '<h2 class="text-center text-info mt-4">Logout Success, Lets Scan Again<h2>')
+		} else {
+			socket.emit('isdelete', '<h2 class="text-center text-danger mt-4">You are have not Login yet!<h2>')
+		}
+	})
+
+	socket.on('scanqr', () => {
+		if (fs.existsSync('./wabot-session.json') && conn.state == 'open') {
+			io.emit('authenticated', conn.user.name + " </br>(" + conn.user.jid + ")")
+			socket.emit('message', 'Sudah Konek!')
+		} else {
+			io.emit('loader', '')
+			conn.on("qr", (qr) => {
+				qrcode.toDataURL(qr, (err, url) => {
+					socket.emit('message', 'QR Code received, scan please!')
+					socket.emit("qr", url);
+				});
+			});
+			socket.emit('message', 'Please wait..')
+			connect()
+		}
+	})
+	socket.on('cekstatus', () => {
+		if (fs.existsSync('./wabot-session.json') && conn.state == 'open') {
+			io.emit('isdelete', '<h2 class="text-center text-primary mt-4">Your whatsapp is Running!</h2>')
+			io.emit('message', 'connected!')
+		} else {
+			io.emit('isdelete', '<h2 class="text-center text-danger mt-4">Your whatsapp is not Running!,Scan Now!<h2>')
+			io.emit('message', 'Whatsapp tidak terhubng!')
+		}
+	})
 });
 
 server.listen(8000, function(){
