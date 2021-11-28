c633dfeb6a5a3c531f4eb6f0dfbe1edf5f491f12
Notdet3cted <whoa75039@gmail.com>
2021-11-28 20:11:06 +0700

    tambah send

--- main.js
+++ main.js
@@ -10,14 +10,21 @@ const {
 	ProxyAgent,
 	waChatKey,
 } = require("@adiwajshing/baileys");
-var qrcode = require('qrcode');
-const express = require('express');
-const app = express();
-const fs = require("fs");
 const http = require("http");
+const https = require("https");
+var qrcode = require('qrcode');
+const fs = require("fs");
+const { body, validationResult } = require('express-validator');
+const express = require('express');
+// const axios = require("axios");
+const app = express();
 const server = http.createServer(app);
 const socketIO = require('socket.io');
+const { phoneNumberFormatter } = require('./formatter');
 const io = socketIO(server);
+// const request = require("request");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 const conn = new WAConnection();
 
 app.get('/', (req, res, next) => {
@@ -30,7 +37,10 @@ async function connect() {
 	const authInfo = conn.base64EncodedAuthInfo(); // get all the auth info we need to restore this session
 	fs.writeFileSync('./wabot-session.json', JSON.stringify(authInfo, null, '\t'))
 	console.log(conn.user.name + " </br>(" + conn.user.jid + ")");
-	io.emit('authenticated', conn.user.name + " </br>(" + conn.user.jid + ")")
+	io.emit('authenticated', {
+		name: conn.user.name,
+		  id: conn.user.jid
+		});
 }
 
 connect().catch((err) => {
@@ -40,8 +50,11 @@ connect().catch((err) => {
 io.on("connection", (socket) => {
 	socket.on('ready', () => {
 		if (fs.existsSync('./wabot-session.json') && conn.state == 'open') {
-			io.emit('authenticated', conn.user.name + " </br>(" + conn.user.jid + ")")
-			socket.emit('message', 'Sudah konek!')
+			io.emit('authenticated', {
+				name: conn.user.name,
+				  id: conn.user.jid
+				});
+			socket.emit('message', 'Perangkat sudah terhubung')
 		} else {
 			io.emit('loader', '')
 			socket.emit('message', 'Please wait..')
@@ -52,7 +65,6 @@ io.on("connection", (socket) => {
 	conn.on("qr", (qr) => {
 		socket.emit('message', 'Getting QR Code')
 		qrcode.toDataURL(qr, (err, url) => {
-			socket.emit('message', 'QR Code received, scan please!')
 			socket.emit("qr", url);
 		});
 	});
@@ -62,16 +74,27 @@ io.on("connection", (socket) => {
 			conn.close()
 			conn.clearAuthInfo();
 			fs.unlinkSync("./wabot-session.json");
-			socket.emit('isdelete', '<h2 class="text-center text-info mt-4">Logout Success, Lets Scan Again<h2>')
+			socket.emit('isdelete',{
+				icon: 'fas fa-check-double',
+				bg: 'success',
+				pesan: '<h2 class="text-center text-success mt-4">Logout berhasil...</h2>'
+			})
 		} else {
-			socket.emit('isdelete', '<h2 class="text-center text-danger mt-4">You are have not Login yet!<h2>')
+			socket.emit('isdelete',{
+				icon: 'fas fa-times',
+				bg: 'danger',
+				pesan: '<h2 class="text-center text-danger mt-4">Whatsapp belum terhubung...</h2>'
+			})
 		}
 	})
 
 	socket.on('scanqr', () => {
 		if (fs.existsSync('./wabot-session.json') && conn.state == 'open') {
-			io.emit('authenticated', conn.user.name + " </br>(" + conn.user.jid + ")")
-			socket.emit('message', 'Sudah Konek!')
+			io.emit('authenticated', {
+				name: conn.user.name,
+				  id: conn.user.jid
+				});
+				socket.emit('message', 'Perangkat sudah terhubung')
 		} else {
 			io.emit('loader', '')
 			conn.on("qr", (qr) => {
@@ -86,13 +109,85 @@ io.on("connection", (socket) => {
 	})
 	socket.on('cekstatus', () => {
 		if (fs.existsSync('./wabot-session.json') && conn.state == 'open') {
-			io.emit('isdelete', '<h2 class="text-center text-primary mt-4">Your whatsapp is Running!</h2>')
-			io.emit('message', 'connected!')
+			io.emit('isdelete',{
+				icon: 'fas fa-check-double',
+				bg: 'success',
+				pesan: '<h2 class="text-center text-success mt-4">Whatsapp sudah terhubung...</h2>'
+			})
 		} else {
-			io.emit('isdelete', '<h2 class="text-center text-danger mt-4">Your whatsapp is not Running!,Scan Now!<h2>')
-			io.emit('message', 'Whatsapp tidak terhubng!')
+			io.emit('isdelete',{
+				icon: 'fas fa-times',
+				bg: 'danger',
+				pesan: '<h2 class="text-center text-danger mt-4">Whatsapp belum terhubung...</h2>'
+			})
 		}
 	})
+});
+
+app.post('/send', [
+	body('number').notEmpty(),
+	body('message').notEmpty(),
+], async (req, res) => {
+	const errors = validationResult(req).formatWith(({
+		msg
+	}) => {
+		return msg;
+	});
+
+	if (!errors.isEmpty()) {
+		return res.status(422).json({
+			status: false,
+			message: errors.mapped(),
+			body: req.body.number
+		});
+	}
+	const message = req.body.message;
+	if (req.body.number.length > 15) {
+		var number = req.body.number;
+		conn.sendMessage(number, message, MessageType.text).then(response => {
+			res.status(200).json({
+				status: true,
+				response: response
+			});
+		}).catch(err => {
+			res.status(500).json({
+				status: false,
+				response: err
+			});
+		});
+		return
+	} else {
+		var number = phoneNumberFormatter(req.body.number);
+	}
+	if (fs.existsSync('./wabot-session.json')) {
+		var numberExists = await conn.isOnWhatsApp(number);
+		if (!numberExists) {
+			return res.status(422).json({
+				status: false,
+				message: 'The number is not registered'
+			});
+		}
+		conn.sendMessage(number, message, MessageType.text).then(response => {
+			res.status(200).json({
+				status: true,
+				response: response
+			});
+		}).catch(err => {
+			res.status(500).json({
+				status: false,
+				response: err
+			});
+		});
+
+	} else {
+		res.writeHead(401, {
+			'Content-Type': 'application/json'
+		});
+		res.end(JSON.stringify({
+			status: false,
+			message: 'Please scan the QR before use the API'
+		}));
+	}
 });
 
 server.listen(8000, function(){
